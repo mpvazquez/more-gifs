f95567db878e2ef84c02b6434dd621e2970efee2
Marco Vazquez <marcopvazquez@Marcos-MacBook-Pro.local>
2018-01-04 14:12:22 -0500

    Refactor clientside scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,40 @@
 	'use strict';
 
 	var offset = 0;
+	var searchPath = null;
+
+	function initMasonry() {
+		var gridElement = document.querySelector('.grid');
+
+		var masonry = new Masonry(gridElement, {
+			columnWidth: 250,
+			fitWidth: true,
+			gutter: 2,
+			itemSelector: '.grid-item'
+		});
+	}
+
+	function makeRequest(event) {
+		event.preventDefault();
+
+		offset += 40;
+
+		var xhr = new XMLHttpRequest();
+		var url = '/get?offset=' + offset;
+
+		if (searchPath) {
+			url += '&query=' + searchPath;
+		}
+
+		xhr.open('GET', url, true);
+		xhr.responseType = 'text';
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+				console.log(xhr.responseText);
+			}
+		}
+		xhr.send();
+	}
 
 	function renderSearchHistory(searchHistory) {
 		var historyContainer = document.getElementById('search-history-container');
@@ -30,8 +64,8 @@
 		}
 	}
 
-	function setEventListeners(searchPath) {
-		var getButton = document.getElementById('get-button');
+	function setEventListeners() {
+		var loadMoreButton = document.getElementById('get-button');
 		var searchButton = document.getElementById('search-button');
 		var searchInput = document.getElementById('search-input');
 
@@ -43,25 +77,7 @@
 			}
 		}
 
-		getButton.addEventListener('click', function(event) {
-			offset += 40;
-
-			var xhr = new XMLHttpRequest();
-			var url = '/get?offset=' + offset;
-
-			if (searchPath) {
-				url += '&query=' + searchPath;
-			}
-
-			xhr.open('GET', url, true);
-			xhr.responseType = 'text';
-			xhr.onreadystatechange = function() {
-				if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-					console.log(xhr.responseText);
-				}
-			}
-			xhr.send();
-		});
+		loadMoreButton.addEventListener('click', makeRequest);
 
 		searchButton.addEventListener('click', searchEvent);
 
@@ -73,23 +89,14 @@
 			}
 		});
 
-		window.addEventListener('load', function() {
-			var gridElement = document.querySelector('.grid');
-
-			var masonry = new Masonry(gridElement, {
-				columnWidth: 250,
-				fitWidth: true,
-				gutter: 2,
-				itemSelector: '.grid-item'
-			});
-		});
+		window.addEventListener('load', initMasonry);
 	}
 
-	function setLocalStorage(searchHistory, searchPath) {
+	function setLocalStorage(searchHistory) {
 		if (searchPath) {
-			searchPath = searchPath.replace(/\+/g, ' ');
-			if (searchHistory.indexOf(searchPath) === -1) {
-				searchHistory.push(searchPath);
+			var search = searchPath.replace(/\+/g, ' ');
+			if (searchHistory.indexOf(search) === -1) {
+				searchHistory.push(search);
 				sessionStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 			}
 		} else {
@@ -98,14 +105,14 @@
 	}
 
 	document.addEventListener("DOMContentLoaded", function() {
-		var searchPath = document.location.pathname.split('/')[2];
+		searchPath = document.location.pathname.split('/')[2];
 
-		setEventListeners(searchPath);
+		setEventListeners();
 
 		if (typeof Storage !== undefined) {
 			var searchHistory = JSON.parse(sessionStorage.getItem('searchHistory')) || [];
 
-			setLocalStorage(searchHistory, searchPath);
+			setLocalStorage(searchHistory);
 			renderSearchHistory(searchHistory);
 		} else {
 			document.getElementById('search-history').remove();
