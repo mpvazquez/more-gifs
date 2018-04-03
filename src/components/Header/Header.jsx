ddb5b00f84ae2e6c9cbde768f85a7235b05d5708
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2018-04-03 14:35:56 -0400

    Update header icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import Icon from 'components/Icon/Icon';
 
 import styles from './Header.pcss';
 
 const Header = () => {
+  const iconClassname = classnames('em-mostly_sunny', [styles.headerIcon]);
+
   return (
     <header className={styles.headerContainer}>
 			<a className={styles.headerLink}
         href="/"
       >
 				<h1 className={styles.headerH1}>
-					<Icon className="em-mostly_sunny" />
+					<Icon className={iconClassname} />
 					<span> MORE GIFs</span>
 				</h1>
 				<h4 className={styles.headerH4}>
